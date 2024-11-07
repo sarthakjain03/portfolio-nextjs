78790e3f59d232507736789cb141b23e0894da3f
Sarthak <jainsarthak2003@gmail.com>
2024-11-08 01:22:03 +0530

    Added basic animation to project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { techStack } from "@/data";
 import TechTab from "./TechTab";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import { Launch, GitHub } from "@mui/icons-material";
 
@@ -19,41 +19,54 @@ export default function ProjectCard({
   const [activeCard, setActiveCard] = useState(false);
 
   return (
-    <div className="flex flex-col border border-white shadow shadow-white rounded-2xl overflow-hidden">
-      <Image src={imgUrl} alt="project image" height={320} width={480} />
-      <div className="flex flex-col gap-3 px-5 py-4 max-w-[480px]">
-        <div className="flex justify-between items-center">
-          <p
-            onClick={() => setActiveCard((prev) => !prev)}
-            className="font-ubuntu font-semibold text-3xl cursor-pointer"
-          >
-            {title}
-          </p>
-          <div className="flex gap-3">
-            <Link target="_blank" href={liveLink}>
-              <Launch />
-            </Link>
-            <Link target="_blank" href={githubRepo}>
-              <GitHub />
-            </Link>
-          </div>
+    <motion.div className="flex flex-col gap-4 border border-white shadow shadow-white rounded-2xl overflow-hidden">
+      <motion.div>
+        <Image src={imgUrl} alt="project image" height={320} width={480} />
+      </motion.div>
+      <motion.div
+        className={`flex justify-between items-center px-5 ${
+          activeCard ? "pb-0" : "pb-4"
+        }`}
+      >
+        <p
+          onClick={() => setActiveCard((prev) => !prev)}
+          className="font-ubuntu font-semibold text-3xl cursor-pointer"
+        >
+          {title}
+        </p>
+        <div className="flex gap-3">
+          <Link target="_blank" href={liveLink}>
+            <Launch />
+          </Link>
+          <Link target="_blank" href={githubRepo}>
+            <GitHub />
+          </Link>
         </div>
+      </motion.div>
+      <AnimatePresence>
         {activeCard && (
-          <div className="flex flex-col gap-4">
+          <motion.div
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: "auto" }}
+            exit={{ opacity: 0, height: 0 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            className="flex flex-col gap-4 px-5 max-w-[480px]"
+          >
             <p
               className="text-xl cursor-pointer"
               onClick={() => setActiveCard(false)}
             >
               {description}
             </p>
-            <div className="flex gap-3">
+            <div className="flex gap-3 pb-4">
               {techArray?.map((tech) => (
                 <TechTab key={tech} title={tech} logo={techStack[tech]} />
               ))}
             </div>
-          </div>
+          </motion.div>
         )}
-      </div>
-    </div>
+      </AnimatePresence>
+      {/* <div className="flex flex-col gap-3 px-5 py-4"></div> */}
+    </motion.div>
   );
 }
--- src/components/Work.js
+++ src/components/Work.js
@@ -5,7 +5,7 @@ export default function Work() {
   return (
     <section
       id="work"
-      className="flex text-white flex-col justify-center items-center gap-10 h-[100vh]"
+      className="flex text-white flex-col justify-center items-center gap-10 min-h-[100vh]"
     >
       <h1 className="text-5xl font-poppins font-medium">Work</h1>
       <div className="flex gap-6">
