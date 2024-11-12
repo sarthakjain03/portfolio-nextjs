2b68a5d9042d528b04a3f94cec0abc21a466b436
Sarthak <jainsarthak2003@gmail.com>
2024-11-12 16:53:13 +0530

    contact section edit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,12 @@ export default function Contact() {
       id="contact"
       className="flex text-white flex-col justify-center items-center gap-10 h-[100vh]"
     >
-      <h1 className="text-4xl font-poppins font-medium">Connect With Me</h1>
+      <div className="flex flex-col justify-center items-center gap-4">
+        <h1 className="text-5xl font-poppins font-medium text-yellow-300">Get in touch!</h1>
+        <p className="font-poppins text-2xl">
+          I would love to connect with you and work on exciting different projects and ideas.
+        </p>
+      </div>
       <div className="flex gap-3">
         <a
           href="https://www.linkedin.com/in/sarthakjain-coder-developer/"
