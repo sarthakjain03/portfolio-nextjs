ffe229b5dc0b259c89e6c0ba644774d7ec6885c3
Sarthak <jainsarthak2003@gmail.com>
2024-11-13 18:22:07 +0530

    added animation to hero-section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,3 +1,4 @@
+'use client'
 import TechTab from "./TechTab";
 import { techStack } from "@/data";
 
@@ -9,22 +10,47 @@ export default function HeroSection() {
       id="hero-section"
       className="flex text-white gap-4 items-center h-[100vh]"
     >
-      <div className="text-center gap-6 flex flex-col">
-        <h1 className="text-6xl font-poppins font-medium">
+      <div className="text-center gap-6 flex flex-col relative">
+        <h1 className="text-6xl font-poppins font-medium z-[2]">
           Welcome human! I&apos;m Sarthak, a{" "}
           <span className="text-yellow-300">Frontend Web Developer</span> from
           India
         </h1>
         <div className="flex gap-2 justify-center">
-          {techs?.map((tech) => (
-            <TechTab key={tech} title={tech} logo={techStack[tech]} />
-          ))}
-          
+          {techs?.map((tab, index) => {
+                const angle = (index / techs.length) * 2 * Math.PI; // Angle in radians
+                const radius = 200;
+                const duration = 16
+                // const x = radius * Math.cos(angle); // X position
+                // const y = radius * Math.sin(angle); // Y position
+
+                return (
+                    <TechTab
+                        key={tab}
+                        title={tab}
+                        logo={techStack[tab]}
+                        radius={radius}
+                        duration={duration}
+                        angleOffset={angle}
+                        animationsActive={true}
+                        styles={{
+                          position: 'absolute',
+                          left: `calc(43%)`,
+                          top: `calc(50%)`,
+                          // left: `calc(43% + ${x})`,
+                          // top: `calc(50% + ${y})`,
+                          width: 136
+                        }}
+                    />
+                )
+            })}
         </div>
+        {/* <div className="flex gap-2 justify-center">
+          {techs?.map((tech, index) => (
+            <TechTab key={tech} title={tech} logo={techStack[tech]} index={index} animationsActive={true} />
+          ))}
+        </div> */}
       </div>
-      {/* <div className='mt-5 rounded-full overflow-hidden'>
-                <Image src="/dp.jpg" alt='Profile picture' width={700} height={750} />
-            </div> */}
     </section>
   );
 }
--- src/components/TechTab.js
+++ src/components/TechTab.js
@@ -1,10 +1,41 @@
+'use client'
 import Image from "next/image"
+import { motion } from "framer-motion"
 
-export default function TechTab({title, logo}) {
+export default function TechTab({ title, logo, index, animationsActive = false, radius, angleOffset, duration, styles }) {
+    const tabVariants = {
+        animateUpDown: {
+            y: [-200, 70],
+            x: 0,
+            transition: {
+                y: {
+                    repeat: Infinity,
+                    repeatType: 'reverse',
+                    duration: 7,
+                    ease: 'easeInOut',
+                    delay: index
+                }
+            }
+        },
+        animateInCircle: {
+            x: Array.from({ length: 360 }, (_, i) =>
+                radius * Math.cos((i * Math.PI) / 180 + angleOffset)
+            ),
+            y: Array.from({ length: 360 }, (_, i) =>
+                radius * Math.sin((i * Math.PI) / 180 + angleOffset)
+            ),
+            transition: {
+                repeat: Infinity,
+                duration: duration,
+                ease: "linear"
+            }
+        }
+    }
+
     return (
-        <div className="flex items-center gap-2 text-white font-ubuntu border border-cyan-400 rounded-3xl w-fit py-2 px-3">
+        <motion.div style={styles} variants={tabVariants} animate={animationsActive && "animateInCircle"} className="flex justify-center items-center gap-2 text-white font-ubuntu border border-cyan-400 rounded-3xl w-fit py-2 px-3">
             <Image src={logo} width={20} height={20} alt={title} />
             <p>{title}</p>
-        </div>
+        </motion.div>
     )
 }
