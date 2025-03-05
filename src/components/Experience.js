a6e05f636a492573e1b566ae78e5e5f34bd451c2
Sarthak <jainsarthak2003@gmail.com>
2025-03-05 10:31:35 +0530

    added new internship exp

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -22,7 +22,60 @@ export default function Experience() {
       <h1 className="text-4xl lg:text-5xl font-poppins font-medium">
         Experience
       </h1>
-      <div>
+      <div className="flex flex-col gap-12">
+        <div className="relative overflow-hidden">
+          <div className="flex items-center gap-4 ml-1 lg:ml-0">
+            <Image
+              src="/aegiscovenant_logo.png"
+              alt="aegiscovenant_logo"
+              width={isSmall ? 50 : 60}
+              height={isSmall ? 50 : 60}
+              className="rounded-full"
+            />
+            <h4 className="text-xl lg:text-2xl text-yellow-300">
+              AegisCovenant Pvt Ltd
+            </h4>
+          </div>
+          <div className="flex ml-7">
+            <div className="w-1 h-5 bg-slate-400"></div>
+            <div className="flex relative -left-[0.285rem] pt-4 gap-3">
+              <div className="w-14 h-8 border-b-4 rounded-t-none rounded-bl-full border-b-slate-400 border-l-4 border-l-slate-400"></div>
+              <div className="flex flex-col">
+                <h5 className="font-ubuntu text-xl lg:text-2xl text-[#FFF5E1]">
+                  SDE Intern (Frontend)
+                </h5>
+                <div className="flex flex-col lg:flex-row lg:gap-2 lg:items-center">
+                  <div className="flex gap-2 items-center">
+                    {isSmall && (
+                      <p className="text-3xl font-bold text-[#61677A]">
+                        &middot;
+                      </p>
+                    )}
+                    <p className="text-lg font-bold text-[#61677A]">
+                      Feb 2025 - Jul 2025 (6 m)
+                    </p>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <p className="text-3xl font-bold text-[#61677A]">
+                      &middot;
+                    </p>
+                    <p className="text-lg font-bold text-[#61677A]">
+                      Onsite - Gurgaon, India
+                    </p>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <p className="text-3xl font-bold text-[#61677A]">
+                      &middot;
+                    </p>
+                    <p className="text-lg font-bold text-[#61677A]">
+                      Full-Time
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="relative overflow-hidden">
           <div className="flex items-center gap-4 ml-1 lg:ml-0">
             <Image
